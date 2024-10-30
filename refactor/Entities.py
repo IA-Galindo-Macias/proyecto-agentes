4cb3fdb5b6ec2777b4d4f7ebbf036fb1194b0edc
galindo <egalindo54@uabc.edu.mx>
2024-10-29 19:44:47 -0700

    weqwee

--- refactor/Entities.py
+++ refactor/Entities.py
@@ -1,27 +1,66 @@
+from collections import deque
+import heapq
+import random
+
 class Entities:
-    def __init__(self, color, coord):
+    def __init__(self, color, coord, player=False):
         self.color = color
         self.coord = coord
+        self.player = player
         
-    def update(self, matriz, lista):
+    def update(self, board):
         pass
     
     def draw(self):
-        pass
+        print(self.color, end="")
+
+def heuristica(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+def greedy_bfs_grafo(grafo, inicio, objetivo):
+    visitados = set()
+    cola = []
+    heapq.heappush(cola, (heuristica(inicio, objetivo), inicio, []))
+
+    while cola:
+        _, actual, camino = heapq.heappop(cola)
+        if actual == objetivo:
+            return camino[1] if len(camino) > 1 else actual
+        if actual not in visitados:
+            visitados.add(actual)
+            for vecino in grafo.get(actual, []):
+                if vecino not in visitados:
+                    nuevo_camino = camino + [actual]
+                    heapq.heappush(cola, (heuristica(vecino, objetivo), vecino, nuevo_camino))
     
+    return inicio
+
+
+class Fantasma(Entities):
+    
+    def update(self, board):
+        ent = [entity for entity in board.entities if entity.player]
+        nuevo_pos = greedy_bfs_grafo(board.grafo_adyacencias, self.coord, ent[0].coord)    
+        
+        self.coord = self.mover_fantasma(nuevo_pos, board.grafo_adyacencias)
+    
+    def mover_fantasma(self, nueva_pos, grafo):
+        if nueva_pos in grafo:
+            return nueva_pos
+        else:
+            return self.coord
+
     
 class Pacman(Entities):
     
     def __init__(self, coord):
         color = "\033[33m██\033[0m"
-        super().__init__(color, coord)
+        is_player = True
+        super().__init__(color, coord, is_player)
         
     def update(self, board):
         direccion = input("w,a,s,d para mover a Pac-Man: ")
         self.mover(board.tablero, direccion)
-        
-    def draw(self):
-        print(self.color, end="")
         
     def mover(self,matriz, direccion):
         x, y = self.coord
